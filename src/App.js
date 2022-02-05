057bedb8d597c282d46201b74e1e940eb6e6647d
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-02-05 14:54:37 +0100

    Layout setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Layout, Typography, Space, Switch } from 'antd';
+import { Navbar, Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails } from './components';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
     </div>
-  );
-}
+    <div className="main">
+      <Layout>
+        <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}></Route>
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />}></Route>
+              <Route exact path="/news" element={<News />}>News</Route>
+            </Routes>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            {' '}Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
